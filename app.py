5c4ac6f5743763d8ee758bc53071944ca80d55a5
hwaseem04 <hajawaseem4@gmail.com>
2024-06-13 12:16:19 +0400

    change model

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def get_openai_response(prompt):
     while attempt < max_retries:
         try:
             response = client.chat.completions.create(messages=prompt,
-                                                        model='gpt-4-turbo')
+                                                        model='gpt-4o')
             return response.choices[0].message.content
         except Exception as e:
             print(f"An error occurred: {str(e)}")
